b0191b782ec509214fe1358a6c63c1c4b056363e
Valerio De Caro <43961074+vdecaro@users.noreply.github.com>
2025-05-08 16:41:52 +0200

    fix: Issue with tee in .bat

--- run.bat
+++ run.bat
@@ -19,14 +19,15 @@ IF "%ARG%"=="touch" (
     REM Start follow-touch in foreground
     echo Starting follow-touch application in foreground...
     call conda activate follow-touch-env
-    python -u follow-touch/app.py 2>&1 | tee follow-touch.log
+    powershell -Command "python -u follow-touch/app.py 2>&1 | Tee-Object -FilePath follow-touch.log"
 ) ELSE IF "%ARG%"=="neural" (
     REM Start neural-model in foreground
     echo Starting neural-model application in foreground...
     call conda activate neural-model-env
-    python -u -m streamlit run neural-model/app.py | tee neural-model.log
+    powershell -Command "python -u -m streamlit run neural-model/app.py 2>&1 | Tee-Object -FilePath neural-model.log"
 ) ELSE (
     echo ❌ Invalid argument. Use "touch" or "neural".
     exit /b 1
 )
+
 endlocal
